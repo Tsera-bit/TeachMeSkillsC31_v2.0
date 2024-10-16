81d0b2dbdf28dc3c9208ad9ffd5c04c73588eaeb
Aliaksei Tseralchuk <arxangel.by@gmail.com>
2024-10-16 23:45:55 +0300

    homework complete

--- src/lessons_17/MyMain.java
+++ src/lessons_17/MyMain.java
@@ -3,7 +3,10 @@ package lessons_17;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class MyMain {
     public static void main(String[] args) {
@@ -12,6 +15,11 @@ public class MyMain {
         // task 2 -----------------------------------------------------------
         filterPositiveValue(new int[]{-1, 2, 4, 6, -8, -10, 0, 32});
         // task 3 -----------------------------------------------------------
+        System.out.printf("%.2f%n", exchangeFunction(100));
+        // task 4 -----------------------------------------------------------
+        exchangeConsumer(550.95);
+        // task 5 -----------------------------------------------------------
+        System.out.println(reversStr());
     }
     static void calculateCentenary() {
         System.out.print("Введите дату рождения в формате dd-MM-yyyy: ");
@@ -35,5 +43,18 @@ public class MyMain {
         }
         System.out.println("Положительными числами являются: " + result);
     }
-
+    static double exchangeFunction(double byn) {
+        Function<Double, Double> exchange = x -> x / 3.3;
+        return exchange.apply(byn);
+    }
+    static void exchangeConsumer(double byn) {
+        Consumer<Double> exchange = x -> System.out.printf("%.2f%n", (x / 3.3));
+        exchange.accept(byn);
+    }
+    static String reversStr() {
+        Supplier<String> revers = () -> {
+            return new StringBuilder(new Scanner(System.in).nextLine()).reverse().toString();
+        };
+        return revers.get();
+    }
 }
